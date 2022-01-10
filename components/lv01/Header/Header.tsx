abbb7c2fe87af8eb3e83ac55b68458f983a7ca88
manuel londoño <mLondono@q10.com>
2022-01-10 18:01:19 -0500

    feat: add media query to Header Component

--- components/lv01/Header/Header.tsx
+++ components/lv01/Header/Header.tsx
@@ -1,11 +1,12 @@
 import Style from "./Header.module.scss";
 import CustomMenuBurgerIcon from "../Icons/CustomMenuBurgerIcon/CustomMenuBurgerIcon";
 import CustomSettingsIcon from "../Icons/CustomSettingsIcon/CustomSettingsIcon";
+import Link from "next/link";
 export default function Header() {
   return (
     <header className={Style.Header}>
       {/* Mobile Components */}
-      <CustomMenuBurgerIcon />
+      <CustomMenuBurgerIcon className={Style.Header_MenuBurgerIcon} />
 
       <h1 className={Style.Header_title}>
         Cronómetro Online - Reloj-Alarma-Clone.es
@@ -13,8 +14,10 @@ export default function Header() {
       {/* Mobile Components */}
 
       {/* Desktop  Components*/}
-      <h2>Reloj-Alarma-Clone.es</h2>
-      <h3>Días festivos</h3>
+      <h2 className={Style.Header_logo}>Reloj-Alarma-Clone.es</h2>
+      <Link href={""}>
+        <a className={Style.Header_days}>Días festivos</a>
+      </Link>
       {/* Desktop  Components*/}
 
       {/* Global Components */}
--- components/lv01/Icons/CustomMenuBurgerIcon/CustomMenuBurgerIcon.tsx
+++ components/lv01/Icons/CustomMenuBurgerIcon/CustomMenuBurgerIcon.tsx
@@ -1,8 +1,23 @@
 import Style from "./CustomMenuBurgerIcon.module.scss";
 
-export default function CustomMenuBurgerIcon() {
+interface CustomMenuBurgerIconInterface {
+  className?: string;
+  onClick?: () => void;
+}
+
+export default function CustomMenuBurgerIcon(
+  props: CustomMenuBurgerIconInterface
+) {
+  const {
+    className = "",
+    onClick = () => console.log("Default onClick CustomMenuBurgerIcon"),
+  } = props;
+
   return (
-    <div className={Style.MenuBurgerIcon}>
+    <div
+      className={`${Style.MenuBurgerIcon} ${className}`}
+      onClick={() => onClick && onClick()}
+    >
       <span></span>
       <span></span>
       <span></span>
